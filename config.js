f93cf4efaf4d1ec189fa61c7a7f5c57a44dd84e3
hungmeomeo <95847972+hungmeomeo@users.noreply.github.com>
2022-02-05 16:18:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     introDesc: `
     Trái đất hàng triệu con người                             Nhưng sao chỉ có một người tôi yêu🥰`,
     btnIntro: '^^HiHi^^',
-    desc: 'Tớ luôn yêu cậu bằng thứ tình cảm chân thành nhất, "cậu có làm NY tớ không?" từ lâu tớ đã muốn hỏi cậu điều này nhưng không dám vì đứng trước người con gái mình yêu trái tim tớ như dừng nhịp🥰',
+    desc: 'Tớ luôn yêu cậu bằng thứ tình cảm chân thành nhất, "cậu có làm NY tớ không?" 🥰',
     
     btnYes: 'Chắc chắn mình sẽ chấp nhận nhé',
     btnNo: 'Xin lỗi nhưng chúng ta không hợp nhau đâu',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho người ấy <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Tớ biết mà 🥰. Tớ biết cậu sẽ chấp nhận đó 😘😘',
+    messDesc: '',
+    btnAccept: '',
     messLink: 'https://github.com/valentine/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
