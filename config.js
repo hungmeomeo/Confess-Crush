0e52ee1ff5397b91fb977cd915bd5c7453d0a433
hungmeomeo <95847972+hungmeomeo@users.noreply.github.com>
2022-02-06 10:07:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const CONFIG = {
     
     btnYes: 'Oki nha 😘',
     btnNo: 'Không nhé 🙂',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Thế giới hơn 8 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho người ấy <3',
-    reply: 'I love you too',
+    reply: 'I love you',
     mess: 'Tớ biết mà 🥰. Tớ biết cậu sẽ chấp nhận đó 😘😘',
     messDesc: '',
     btnAccept: '',
